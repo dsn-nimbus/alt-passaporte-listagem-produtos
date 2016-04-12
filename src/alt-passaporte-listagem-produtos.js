b253d0c4a03929035705e1c8d93d1c2ce41d3337
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-04-12 12:31:45 -0300

    cria método para consumo de endpoint apenas de produtos habilitados

--- src/alt-passaporte-listagem-produtos.js
+++ src/alt-passaporte-listagem-produtos.js
@@ -2,77 +2,93 @@
   "use strict";
 
   ng.module('alt.passaporte-listagem-produtos', ['alt.passaporte-informacoes-autorizacao'])
-    .config(['$httpProvider', function($httpProvider) {
-      $httpProvider.defaults.withCredentials = true;
-    }])
-    .provider('AltPassaporteUrlBaseListagemProdutos', [function() {
-      this.url = '';
+  .config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.withCredentials = true;
+  }])
+  .provider('AltPassaporteUrlBaseListagemProdutos', [function() {
+    this.url = '';
 
-      this.$get = [function() {
-        return this.url;
-      }];
-    }])
-    .service('AltPassaporteListagemProdutosService', ['$http', '$q', '$filter', 'AltPassaporteAuthorizationInfoService', 'AltPassaporteUrlBaseListagemProdutos', function($http, $q, $filter, AltPassaporteAuthorizationInfoService, AltPassaporteUrlBaseListagemProdutos) {
-      var URL_PASSAPORTE_PRODUTOS = AltPassaporteUrlBaseListagemProdutos + '/passaporte-rest-api/rest/produtos';
+    this.$get = [function() {
+      return this.url;
+    }];
+  }])
+  .service('AltPassaporteListagemProdutosService', ['$http', '$q', '$filter', 'AltPassaporteAuthorizationInfoService', 'AltPassaporteUrlBaseListagemProdutos', function($http, $q, $filter, AltPassaporteAuthorizationInfoService, AltPassaporteUrlBaseListagemProdutos) {
+    var URL_PASSAPORTE_PRODUTOS = AltPassaporteUrlBaseListagemProdutos + '/passaporte-rest-api/rest/produtos/:prop';
 
-      this._altPassaporteAuthorizationInfoService = new AltPassaporteAuthorizationInfoService(AltPassaporteUrlBaseListagemProdutos);
+    this._altPassaporteAuthorizationInfoService = new AltPassaporteAuthorizationInfoService(AltPassaporteUrlBaseListagemProdutos);
 
-      this._parseHttp = function() {
-        return $http.get(URL_PASSAPORTE_PRODUTOS).then(function(p) {
-          return p.data || {};
-        });
-      };
+    this._parseHttp = function(prop) {
+      var url = URL_PASSAPORTE_PRODUTOS;
 
-      this._adicionaPassaporteNoTopoDaLista = function(listaProd, prod, nomePassaporte, indice) {
-        var _nomeProd = ng.lowercase(prod.nome);
+      url = angular.isUndefined(prop) ? url.replace('/:prop', '') : url.replace(':prop', prop);
 
-        if (_nomeProd === nomePassaporte) {
-          listaProd.splice(indice, 1);
-          listaProd.unshift(prod);
+      return $http.get(url).then(function(p) {
+        return p.data || {};
+      });
+    };
 
-          return;
-        }
-      };
+    this._adicionaPassaporteNoTopoDaLista = function(listaProd, prod, nomePassaporte, indice) {
+      var _nomeProd = ng.lowercase(prod.nome);
 
-      this.ordenaPorNome = function(produtosWrapper) {
-          var self = this;
+      if (_nomeProd === nomePassaporte) {
+        listaProd.splice(indice, 1);
+        listaProd.unshift(prod);
 
-          var _produtosWrapper = produtosWrapper || {};
+        return;
+      }
+    };
 
-          _produtosWrapper.habilitados = $filter('orderBy')(produtosWrapper.habilitados, 'nome') || [];
+    this.ordenaPorNome = function(produtosWrapper) {
+      var self = this;
 
-          ng.forEach(_produtosWrapper.habilitados, function(prod, indice) {
-              self._adicionaPassaporteNoTopoDaLista(_produtosWrapper.habilitados, prod, "passaporte admin", indice);
+      var _produtosWrapper = produtosWrapper || {};
+
+      _produtosWrapper.habilitados = $filter('orderBy')(produtosWrapper.habilitados, 'nome') || [];
+
+      ng.forEach(_produtosWrapper.habilitados, function(prod, indice) {
+        self._adicionaPassaporteNoTopoDaLista(_produtosWrapper.habilitados, prod, "passaporte admin", indice);
+      });
+
+      ng.forEach(_produtosWrapper.habilitados, function(prod, indice) {
+        self._adicionaPassaporteNoTopoDaLista(_produtosWrapper.habilitados, prod, "passaporte", indice);
+      });
+
+      return _produtosWrapper;
+    };
+
+    this._getProdutos = function(prop) {
+      var self = this;
+
+      return $q.all([this._altPassaporteAuthorizationInfoService.getToken(), this._parseHttp(prop)])
+        .then(function(resultado) {
+          var _token = resultado[0];
+          var _produtosWrapper = resultado[1];
+
+          ng.forEach(_produtosWrapper.habilitados, function(ph) {
+            var _separadorInicial = /\?/.test(ph.urlAutenticacao) ? '&' : '?';
+            ph.urlAutenticacao = ph.urlAutenticacao + _separadorInicial + 'info='
+            + _token + '&idProduto='
+            + ph.chave;
           });
 
-          ng.forEach(_produtosWrapper.habilitados, function(prod, indice) {
-              self._adicionaPassaporteNoTopoDaLista(_produtosWrapper.habilitados, prod, "passaporte", indice);
-          });
+        return self.ordenaPorNome(_produtosWrapper);
+      });
+    }
 
-          return _produtosWrapper;
-      };
+    this.getProdutos = function getProdutos() {
+      // retorna produtos habilitados e disponívels
+      // {habilitados: [], disponiveis: []}
 
-      this.getProdutos = function getProdutos() {
-        var self = this;
+      return this._getProdutos(undefined);
+    };
 
-        return $q.all([this._altPassaporteAuthorizationInfoService.getToken(), this._parseHttp()])
-                 .then(function(resultado) {
-                   var _token = resultado[0];
-                   var _produtosWrapper = resultado[1];
+    this.getProdutosHabilitados = function getProdutos() {
+      // retorna apenas os produtos habilitados
+      // {habilitados: []}
 
-                   ng.forEach(_produtosWrapper.habilitados, function(ph) {
-                          var _separadorInicial = /\?/.test(ph.urlAutenticacao) ? '&' : '?';
-                          ph.urlAutenticacao = ph.urlAutenticacao + _separadorInicial + 'info='
-                                                                                      + _token + '&idProduto='
-                                                                                      + ph.chave;
-                   });
+      return this._getProdutos('habilitados');
+    };
 
-                   return self.ordenaPorNome(_produtosWrapper);
-                 })
-                 .catch(function(erro) {
-                   return $q.reject(erro);
-                 });
-     };
-    }]);
+  }]);
 
 }(angular));
--- tests/alt-passaporte-listagem-produtos_test.js
+++ tests/alt-passaporte-listagem-produtos_test.js
@@ -5,6 +5,7 @@ describe('alt.passaporte-listagem-produtos', function() {
   var TOKEN = 'abc123123';
   var BASE_URL_PASSAPORTE = 'http://123.com';
   var URL_PASSAPORTE_PRODUTOS = BASE_URL_PASSAPORTE + '/passaporte-rest-api/rest/produtos';
+  var URL_PASSAPORTE_PRODUTOS_HABILITADOS = BASE_URL_PASSAPORTE + '/passaporte-rest-api/rest/produtos/habilitados';
   var URL_PASSAPORTE_TOKEN = BASE_URL_PASSAPORTE + '/passaporte-rest-api/rest/authorization/token';
 
   beforeEach(module('alt.passaporte-listagem-produtos', function(AltPassaporteUrlBaseListagemProdutosProvider, $httpProvider) {
@@ -278,7 +279,7 @@ describe('alt.passaporte-listagem-produtos', function() {
       expect(_resultado.habilitados[3]).toEqual({nome: 'b'});
       expect(_resultado.habilitados[4]).toEqual({nome: 'c'});
       expect(_resultado.habilitados[5]).toEqual({nome: 'x'});
-    });    
+    });
 
     it('deve ordenar os produtos por nome - existem ambos passaporte admin e passaporte', function() {
       var _produtos = {
@@ -302,4 +303,174 @@ describe('alt.passaporte-listagem-produtos', function() {
       expect(_resultado.habilitados[5]).toEqual({nome: 'x'});
     });
   })
+
+  describe('getProdutosHabilitados', function() {
+    it('deve tentar buscar os produtos, mas o servidor retorna erro', function() {
+      spyOn(_AltPassaporteListagemProdutosService._altPassaporteAuthorizationInfoService, 'getToken').and.callFake(function() {
+        return _q.when(TOKEN);
+      });
+
+      _httpBackend.expectGET(URL_PASSAPORTE_PRODUTOS_HABILITADOS).respond(400);
+
+      _AltPassaporteListagemProdutosService
+        .getProdutosHabilitados()
+        .then(function() {
+          expect(true).toBe(false);
+        })
+        .catch(function(erro) {
+          expect(erro).toBeDefined();
+        });
+
+      _httpBackend.flush();
+    });
+
+    it('deve buscar as informações corretamente', function() {
+      spyOn(_AltPassaporteListagemProdutosService._altPassaporteAuthorizationInfoService, 'getToken').and.callFake(function() {
+        return _q.when(TOKEN);
+      });
+
+      _httpBackend.expectGET(URL_PASSAPORTE_PRODUTOS_HABILITADOS).respond(200, {habilitados: [], disponiveis: []});
+
+      _AltPassaporteListagemProdutosService
+        .getProdutosHabilitados()
+        .then(function(p) {
+          expect(p).toBeDefined();
+        })
+        .catch(function() {
+          expect(true).toBe(false);
+        });
+
+      _httpBackend.flush();
+    });
+
+    it('deve buscar as informações corretamente e fazer o parse das urls com os tokens - urlAutenticacao sem query string', function() {
+      var _produtosHabilitadosResposta = [
+        {urlAutenticacao: 'http://site1.com/auth', chave: '987654321', url: 'http://site1.com'},
+        {urlAutenticacao: 'http://site2.com/auth', chave: '123456789', url: 'http://site2.com'},
+        {urlAutenticacao: 'http://site3.com/auth', chave: '654987321', url: 'http://site3.com'}
+      ];
+
+      var _produtosDisponiveisResposta = [
+        {url: 'http://site1.com'},
+        {url: 'http://site2.com'},
+        {url: 'http://site3.com'}
+      ];
+
+      var _produtosHabilitadosParsed = [
+        {urlAutenticacao: 'http://site1.com/auth?info=abc123123&idProduto=987654321', chave: '987654321', url: 'http://site1.com'},
+        {urlAutenticacao: 'http://site2.com/auth?info=abc123123&idProduto=123456789', chave: '123456789', url: 'http://site2.com'},
+        {urlAutenticacao: 'http://site3.com/auth?info=abc123123&idProduto=654987321', chave: '654987321', url: 'http://site3.com'}
+      ];
+
+      var _produtoWrapper = {
+        habilitados: _produtosHabilitadosResposta,
+        disponiveis: _produtosDisponiveisResposta
+      };
+
+      spyOn(_AltPassaporteListagemProdutosService._altPassaporteAuthorizationInfoService, 'getToken').and.callFake(function() {
+        return _q.when(TOKEN);
+      });
+
+      _httpBackend.expectGET(URL_PASSAPORTE_PRODUTOS_HABILITADOS).respond(200, _produtoWrapper);
+
+      _AltPassaporteListagemProdutosService
+        .getProdutosHabilitados()
+        .then(function(pw) {
+          expect(pw.habilitados).toEqual(_produtosHabilitadosParsed);
+          expect(pw.disponiveis).toEqual(_produtosDisponiveisResposta);
+        })
+        .catch(function(erro) {
+          expect(true).toBe(false);
+        });
+
+      _httpBackend.flush();
+    });
+
+   it('deve buscar as informações corretamente e fazer o parse das urls com os tokens - urlAutenticacao com query string', function() {
+      var _produtosHabilitadosResposta = [
+        {urlAutenticacao: 'http://site1.com/auth?qqcoisa', chave: '987654321', url: 'http://site1.com'},
+        {urlAutenticacao: 'http://site2.com/auth?qqcoisa', chave: '123456789', url: 'http://site2.com'},
+        {urlAutenticacao: 'http://site3.com/auth?qqcoisa', chave: '654987321', url: 'http://site3.com'}
+      ];
+
+      var _produtosDisponiveisResposta = [
+        {url: 'http://site1.com'},
+        {url: 'http://site2.com'},
+        {url: 'http://site3.com'}
+      ];
+
+      var _produtosHabilitadosParsed = [
+        {urlAutenticacao: 'http://site1.com/auth?qqcoisa&info=abc123123&idProduto=987654321', chave: '987654321', url: 'http://site1.com'},
+        {urlAutenticacao: 'http://site2.com/auth?qqcoisa&info=abc123123&idProduto=123456789', chave: '123456789', url: 'http://site2.com'},
+        {urlAutenticacao: 'http://site3.com/auth?qqcoisa&info=abc123123&idProduto=654987321', chave: '654987321', url: 'http://site3.com'}
+      ];
+
+      var _produtoWrapper = {
+        habilitados: _produtosHabilitadosResposta,
+        disponiveis: _produtosDisponiveisResposta
+      };
+
+      spyOn(_AltPassaporteListagemProdutosService._altPassaporteAuthorizationInfoService, 'getToken').and.callFake(function() {
+        return _q.when(TOKEN);
+      });
+
+      _httpBackend.expectGET(URL_PASSAPORTE_PRODUTOS_HABILITADOS).respond(200, _produtoWrapper);
+
+      _AltPassaporteListagemProdutosService
+        .getProdutosHabilitados()
+        .then(function(pw) {
+          expect(pw.habilitados).toEqual(_produtosHabilitadosParsed);
+          expect(pw.disponiveis).toEqual(_produtosDisponiveisResposta);
+        })
+        .catch(function(erro) {
+          expect(true).toBe(false);
+        });
+
+      _httpBackend.flush();
+    });
+
+    it('deve buscar as informações corretamente e fazer o parse das urls com os tokens - urlAutenticacao com query string', function() {
+      var _produtosHabilitadosResposta = [
+        {urlAutenticacao: 'http://site1.com/auth?qqcoisa', chave: '987654321', url: 'http://site1.com'},
+        {urlAutenticacao: 'http://site2.com/auth?qqcoisa', chave: '123456789', url: 'http://site2.com'},
+        {urlAutenticacao: 'http://site3.com/auth?qqcoisa', chave: '654987321', url: 'http://site3.com'}
+      ];
+
+      var _token = {
+        token: 'abc123123'
+      };
+
+      var _produtosDisponiveisResposta = [
+        {url: 'http://site1.com'},
+        {url: 'http://site2.com'},
+        {url: 'http://site3.com'}
+      ];
+
+      var _produtosHabilitadosParsed = [
+        {urlAutenticacao: 'http://site1.com/auth?qqcoisa&info=abc123123&idProduto=987654321', chave: '987654321', url: 'http://site1.com'},
+        {urlAutenticacao: 'http://site2.com/auth?qqcoisa&info=abc123123&idProduto=123456789', chave: '123456789', url: 'http://site2.com'},
+        {urlAutenticacao: 'http://site3.com/auth?qqcoisa&info=abc123123&idProduto=654987321', chave: '654987321', url: 'http://site3.com'}
+      ];
+
+      var _produtoWrapper = {
+        habilitados: _produtosHabilitadosResposta,
+        disponiveis: _produtosDisponiveisResposta
+      };
+
+      _httpBackend.expectGET(URL_PASSAPORTE_TOKEN).respond(200, _token);
+      _httpBackend.expectGET(URL_PASSAPORTE_PRODUTOS_HABILITADOS).respond(200, _produtoWrapper);
+
+      _AltPassaporteListagemProdutosService
+        .getProdutosHabilitados()
+        .then(function(pw) {
+          expect(pw.habilitados).toEqual(_produtosHabilitadosParsed);
+          expect(pw.disponiveis).toEqual(_produtosDisponiveisResposta);
+        })
+        .catch(function(erro) {
+          expect(true).toBe(false);
+        });
+
+      _httpBackend.flush();
+    });
+  });
 });
